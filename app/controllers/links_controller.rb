c71550a2b7f495a6c1eb46945a570e1b0b6681d2
Nathan Beers <nbeers22@gmail.com>
2015-08-11 17:50:56 -0400

    Added create action for links and linked it to the current_user

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,14 @@
 class LinksController < InheritedResources::Base
 
+	def create
+		user = current_user
+		# user = User.find(params[:id])
+		@link = current_user.links.create(link_params)
+		if @link.save
+			redirect_to :root
+		end
+	end
+
   private
 
     def link_params
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811183837) do
+ActiveRecord::Schema.define(version: 20150811202633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,10 @@ ActiveRecord::Schema.define(version: 20150811183837) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "links", ["user_id"], name: "index_links_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -76,4 +79,5 @@ ActiveRecord::Schema.define(version: 20150811183837) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "links", "users"
 end
