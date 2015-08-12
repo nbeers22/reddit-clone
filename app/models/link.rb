344a2687189d69d28b74dfeeefd4baf213d6ad4b
Nathan Beers <nbeers22@gmail.com>
2015-08-12 11:05:29 -0400

    refactored link http:/./ addition into a class method

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,12 @@
 class Link < ActiveRecord::Base
 	belongs_to :user, dependent: :destroy
+
+	def self.add_http(link)
+		link.downcase!
+		if link.include?('http://')
+			link
+		else
+			link = 'http://' + link
+		end
+	end
 end
